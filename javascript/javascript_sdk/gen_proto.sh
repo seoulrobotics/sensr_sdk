692834edacfca07f51f86dacdea217dcfbfe7ddc
luke <luke@seoulrobotics.org>
2020-11-12 15:18:26 +0900

    use the new format of output.

--- javascript/javascript_sdk/gen_proto.sh
+++ javascript/javascript_sdk/gen_proto.sh
@@ -2,7 +2,7 @@
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUT_DIR="${SRC_DIR}/js_proto"
-PROTO_DIR="${SRC_DIR}/../../proto/"
+PROTO_DIR="${SRC_DIR}/../../proto"
 if [ ! -d $OUT_DIR ]; then
   mkdir $OUT_DIR
 fi
@@ -10,4 +10,4 @@ fi
 protoc \
   --proto_path=$PROTO_DIR \
   --js_out=import_style=commonjs,binary:$OUT_DIR \
-  $PROTO_DIR/*/*.proto $PROTO_DIR/*/*/*.proto \
+  $PROTO_DIR/*/*.proto \
--- javascript/test/client.js
+++ javascript/test/client.js
@@ -1,14 +1,16 @@
 
 
 var output_msg = require('./js_proto/sensr_proto/output_pb.js');
-var socket;
+var point_msg = require('./js_proto/sensr_proto/point_cloud_pb.js');
+var socket_result;
+var socket_point;
 
 
-  socket = new WebSocket("ws://localhost:5050", ["deflate-frame"]);
-  socket.binaryType = "arraybuffer";
-  socket.addEventListener('message', function (event) {
+  socket_result = new WebSocket("ws://localhost:5050", ["deflate-frame"]);
+  socket_result.binaryType = "arraybuffer";
+  socket_result.addEventListener('message', function (event) {
     //callbackFunc(event.data);
-    var response = output_msg.RegularObjectResponse.deserializeBinary(event.data);
+    var response = output_msg.OutputMessage.deserializeBinary(event.data);
     var server_t = response.getTimeStamp();
     var server_time = (server_t.getSeconds() * 1000) + (server_t.getNanos() / 1000000)
     var client_t = new Date();
@@ -17,11 +19,22 @@ var socket;
     //console.log("Client:" , client_time);
     var diff = client_time - server_time;
     console.log("Diff3:" , diff);
-    // var obj_list = response.getObjectsList();
-    // obj_list.forEach(element => {
-    //     var vel = element.getVelocity();
-    //     console.log(element.getId(), element.getConfidence(), element.getLabel())
-    //     console.log("Velocity:", vel.getX(), vel.getY(), vel.getZ())
-    // });
+    var obj_list = response.getStream().getObjectsList();
+     obj_list.forEach(element => {
+         var point_list = element.getPoints_asU8();
+         console.log(element.getId(), element.getConfidence(), element.getLabel(), point_list.length)
+     });
   });  
+
+  socket_point = new WebSocket("ws://localhost:5051", ["deflate-frame"]);
+  socket_point.binaryType = "arraybuffer";
+  socket_point.addEventListener('message', function (event) {
+    //callbackFunc(event.data);
+    var response = point_msg.PointResult.deserializeBinary(event.data);
+    var obj_list = response.getPointsList();
+     obj_list.forEach(element => {
+         var point_list = element.getPoints_asU8();
+         console.log(element.getId(), point_list.length)
+     });
+  });
 
