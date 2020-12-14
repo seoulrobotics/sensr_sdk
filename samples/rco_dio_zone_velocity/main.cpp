da4f9642634be9933bbfd2d69159740c39505bd4
CY-sr <logan@seoulrobotics.org>
2020-12-14 09:49:01 +0900

    RCO_DIO

--- samples/rco_dio_zone_velocity/main.cpp
+++ samples/rco_dio_zone_velocity/main.cpp
@@ -62,8 +62,9 @@ int set_gpio(int direction, int gpio_idx, int value)
 
 int main(int argc, char *argv[])
 {
-  bool zone_check[4] = {0};
-  std::pair<int ,int> zone_info[4];
+  std::cout << "starting" << std::endl;
+
+  std::pair<int ,float> zone_info[4];
   std::ifstream readFile;
   char strBuffer[1000];
   char *pstrBuffer = NULL;
@@ -82,21 +83,27 @@ int main(int argc, char *argv[])
 		std::strcpy(str_array,str.c_str());
 		char *ptr = strtok(str_array," ");
 		int i=0;
-		int zone_type = atoi(ptr);
+		int zone_id = atoi(ptr);
 		while(ptr != NULL){
 			if(i>=2) break;
 			ptr = std::strtok(NULL," ");
-			if(i==0)zone_info[zone_type].first = atoi(ptr);
-			else if(i==1) zone_info[zone_type].second = atoi(ptr);
+			if(i==0)zone_info[zone_id].first = atoi(ptr);
+			else if(i==1) zone_info[zone_id].second = atof(ptr);
 			i++;		
 		}
 	}
+  } else {
+    std::cout << "File: " << pstrBuffer << " not found" << std::endl;
+    return 0;
   }
   int c, value=UNDEFINED, gpio_idx=UNDEFINED;
   int direction=IN;
   const char *client_address = "localhost"; // change your localhost ip
   int change_value = 2;
-  if(ioperm(AddrPort, 2, 1)) return 0;
+  if(ioperm(AddrPort, 2, 1)) {
+    std::cout << "Could not open port: " << client_address << ":" << AddrPort << std::endl;
+    return 0;
+  }
   else{
 	WriteByte(AddrPort, SIO_UNLOCK_VALUE);
 	usleep(4000);
@@ -118,43 +125,41 @@ int main(int argc, char *argv[])
     OutputMessage message;
     if (client.ReceiveMessage(message))
     {
-      for(int i=0; i<4; i++) zone_check[i] = 0;
-      size_t object_points_size = 0;
-      int tracked_objects_size = 0, non_tracked_objects_size = 0;
-      for(const auto& zone : message.region_of_interest()) {
-	for(const auto& object_zone : zone.object_ids()) {
-		int cal_velocity = 0;
-		for(const auto& object : message.objects()) {
-        		if (object.has_track() && object.track().tracking_reliable() && object_zone == object.id()){
-          			cal_velocity = sqrt(pow(object.track().velocity().x(),2)+pow(object.track().velocity().y(),2)+pow(object.track().velocity().z(),2));
-				if(zone.id() == 0 && cal_velocity > zone_info[0].second) { zone_check[0] = 1;}
-				else if(zone.id() == 0 && cal_velocity <= zone_info[0].second) { zone_check[0] = 0;}
-				if(zone.id() == 1 && cal_velocity > zone_info[1].second) { zone_check[1] = 1;}
-				else if(zone.id() == 1 && cal_velocity <= zone_info[1].second) { zone_check[1] = 0;}
-				if(zone.id() == 2 && cal_velocity > zone_info[2].second) { zone_check[2] = 1;}
-				else if(zone.id() == 2 && cal_velocity <= zone_info[2].second) { zone_check[2] = 0;}
-				if(zone.id() == 3 && cal_velocity > zone_info[3].second) { zone_check[3] = 1;}
-				else if(zone.id() == 3 && cal_velocity <= zone_info[3].second) { zone_check[3] = 0;}
+	  // Reset the zone check for every new message (default is off)
+      int zone_check[4] = {0};
 
-        		} else {
-          			non_tracked_objects_size++;
-        		}
-			for(int i=0; i<4; i++) {
-				if(zone_info[i].first == 0 && zone_info[i].second == 0) continue; 
-				if(zone_check[i]) set_gpio(OUT, zone_info[i].first,0);				
-				else if(!zone_check[i]) set_gpio(OUT, zone_info[i].first, 1);
+      for(int i=0; i<4; i++) zone_check[i] = 0;
+		  size_t object_points_size = 0;
+		  int tracked_objects_size = 0, non_tracked_objects_size = 0;
+		  for(const auto& zone : message.region_of_interest()) {
+			for(const auto& object_zone : zone.object_ids()) {
+				float cal_velocity = 0;
+				for(const auto& object : message.objects()) {
+					if (object.has_track() && object.track().tracking_reliable() && object_zone == object.id()){
+						cal_velocity = sqrt(pow(object.track().velocity().x(),2)+pow(object.track().velocity().y(),2)+pow(object.track().velocity().z(),2));
+						std::cout << "velocity: " << cal_velocity << std::endl;
+						if (zone.id() > 3)
+						  continue;
+						// Keep track of how many objects are violating 
+						zone_check[zone.id()] += cal_velocity > zone_info[zone.id()].second;
+					} else {
+						non_tracked_objects_size++;
+					}
+				}
 			}
-      		}
-      	}
-      }
-      std::cout << "Message received from SENSR!" << std::endl
-                << "Timestamp: " << message.time_stamp().seconds() << " (s) "
-                                 << message.time_stamp().nanos() << " (ns)" << std::endl
-                << "Ground Points: " << message.ground_points().length() / (sizeof(float) * 3) << std::endl
-                << "Object Points: " << object_points_size << std::endl
-                << "Tracked Objects: " << tracked_objects_size << std::endl
-                << "Non Tracked Objects: " << non_tracked_objects_size << std::endl
-                << std::endl;
+		}
+		for(int i=0; i<4; i++) {
+			set_gpio(OUT, zone_info[i].first,zone_check[i]>0);				
+    }
+
+      //std::cout << "Message received from SENSR!" << std::endl
+      //          << "Timestamp: " << message.time_stamp().seconds() << " (s) "
+      //                           << message.time_stamp().nanos() << " (ns)" << std::endl
+      //          << "Ground Points: " << message.ground_points().length() / (sizeof(float) * 3) << std::endl
+      //          << "Object Points: " << object_points_size << std::endl
+      //          << "Tracked Objects: " << tracked_objects_size << std::endl
+      //          << "Non Tracked Objects: " << non_tracked_objects_size << std::endl
+      //          << std::endl;
     }
   }
   WriteByte(AddrPort, SIO_LOCK_VALUE);
