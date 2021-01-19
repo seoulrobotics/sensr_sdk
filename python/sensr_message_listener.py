9c9331336cac534c39fa607e25765215d103d808
thor-sr <thorsteinn@seoulrobotics.org>
2021-01-20 08:53:55 +0900

    Turn python message listener into abstract base class that provides interface for getting output messages and point results

--- python/sensr_message_listener.py
+++ python/sensr_message_listener.py
@@ -3,6 +3,7 @@ import sys
 import websockets
 import asyncio
 from enum import Enum
+from abc import ABCMeta, abstractmethod # Abstract base classes
 
 from sensr_proto.output_pb2 import OutputMessage
 from sensr_proto.point_cloud_pb2 import PointResult
@@ -13,53 +14,61 @@ class ListenerType(Enum):
     POINT_RESULT = 2
     BOTH = 3
 
-class MessageListener:
-    def __init__(self, address, output_dir, listener_type=ListenerType.BOTH, max_message=10):
-        self._output_dir = output_dir
-        self._address = address
-        self._max_message = max_message
-        self._num_received = 0
+class MessageListener(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def __init__(self, address, listener_type=ListenerType.BOTH, output_port = "5050", point_port = "5051", max_message=5):
+        self._address = "ws://" + address
+        self._output_address = self._address + ':' + output_port
+        self._point_address = self._address + ':' + point_port
         self._listener_type = listener_type
 
+        self._max_message = max_message
+        self._num_received = 0
+
     def __del__(self):
-        print('Finished dumping {} messages from SENSR.'.format(self._num_received))
+        print('Finished receiving {} messages from SENSR.'.format(self._num_received))
+
 
     async def _output_stream(self):
-        async with websockets.connect(self._address) as websocket:
+        async with websockets.connect(self._output_address) as websocket:
             while self._num_received < self._max_message:
-                output_message = await websocket.recv() # Receive output messages from SENSR
-                output_fn = os.path.join(self._output_dir, f'frame_{self._num_received:06}.bin')
-                with open(output_fn, 'wb') as fp:
-                    fp.write(output_message)
-                    self._num_received += 1
-                    print('Dumping {} messages from SENSR.'.format(self._num_received))
+                message = await websocket.recv() # Receive output messages from SENSR
+                self._num_received += 1
+                print('Received {} output messages from SENSR.'.format(self._num_received))
 
-    def connect(self):
-        print('Dumping SENSR output to {}...'.format(self._output_dir))    
-        
-        asyncio.get_event_loop().run_until_complete(self._output_stream())
-
-    def on_get_output_message(self):
-        # TODO
-        pass
-
-    def on_get_point_result(self):
-        # TODO
-        pass
-
-
-
-def main():
-
-    output_dir = 'sample_output'
-    address = "ws://localhost:5050"
-
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
-
-    listener = MessageListener(address, output_dir)
-    listener.connect()
+                output = OutputMessage()
+                output.ParseFromString(message)
+                self._on_get_output_message(output)
     
 
-if __name__ == '__main__':
-    main()
+    async def _point_stream(self):
+        async with websockets.connect(self._point_address) as websocket:
+            while self._num_received < self._max_message:
+                message = await websocket.recv() # Receive output messages from SENSR
+                self._num_received += 1
+                print('Received {} point messages from SENSR.'.format(self._num_received))
+
+                points = PointResult()
+                points.ParseFromString(message)
+                self._on_get_point_result(points)
+
+
+    def connect(self):
+        print('Receiving SENSR output from {}...'.format(self._address))    
+        
+        if self._listener_type == ListenerType.OUTPUT_MESSAGE:
+            asyncio.get_event_loop().run_until_complete(self._output_stream())
+
+        if self._listener_type == ListenerType.POINT_RESULT:
+            asyncio.get_event_loop().run_until_complete(self._point_stream())
+
+
+    def _on_get_output_message(self, message):
+        if (self._listener_type == ListenerType.OUTPUT_MESSAGE or self._listener_type == ListenerType.BOTH):
+            raise Exception('on_get_output_message() needs to be implemented in the derived class')
+
+    def _on_get_point_result(self, message):
+        if (self._listener_type == ListenerType.POINT_RESULT or self._listener_type == ListenerType.BOTH):
+            raise Exception('on_get_point_result() needs to be implemented in the derived class')
+
