372617746eb124bf97e0b937aac1882e9361b2cf
thor-sr <thorsteinn@seoulrobotics.org>
2021-01-19 16:26:32 +0900

    Move functionality into class

--- python/sensr_message_listener.py
+++ python/sensr_message_listener.py
@@ -3,30 +3,44 @@ import sys
 import websockets
 import asyncio
 
-output_dir = 'sample_output'
-num_received = 0
-max_message = 1000
+class MessageListener:
+    def __init__(self, output_dir, address, max_message=100):
+        self._output_dir = output_dir
+        self._address = address
+        self._max_message = max_message
+        self._num_received = 0
+
+    def __del__(self):
+        print('Finished dumping {} messages from SENSR.'.format(self._num_received))
+
+    async def _output_stream(self):
+        async with websockets.connect(self._address) as websocket:
+            while self._num_received < self._max_message:
+                output_message = await websocket.recv() # Receive output messages from SENSR
+                output_fn = os.path.join(self._output_dir, f'frame_{self._num_received:06}.bin')
+                with open(output_fn, 'wb') as fp:
+                    fp.write(output_message)
+                    self._num_received += 1
+                    print('Dumping {} messages from SENSR.'.format(self._num_received))
+
+    def connect(self):
+        print('Dumping SENSR output to {}...'.format(self._output_dir))    
+        
+        asyncio.get_event_loop().run_until_complete(self._output_stream())
 
 
-async def output_stream():
-    uri = "ws://localhost:5050"
-    async with websockets.connect(uri) as websocket:
-        global num_received
-        while num_received < max_message:
-            output_message = await websocket.recv() # Receive output messages from SENSR
-            output_fn = os.path.join(output_dir, f'frame_{num_received:06}.bin')
-            with open(output_fn, 'wb') as fp:
-                fp.write(output_message)
-                num_received += 1
-                print('Dumping {} messages from SENSR.'.format(num_received))
 
 def main():
+
+    output_dir = 'sample_output'
+    address = "ws://localhost:5050"
+
     if not os.path.isdir(output_dir):
         os.makedirs(output_dir)
 
-    print('Dumping SENSR output to {}...'.format(output_dir))    
-    asyncio.get_event_loop().run_until_complete(output_stream())
-    print('Finished dumping {} messages from SENSR.'.format(num_received))
+    listener = MessageListener(output_dir, address)
+    listener.connect()
+    
 
 if __name__ == '__main__':
     main()
