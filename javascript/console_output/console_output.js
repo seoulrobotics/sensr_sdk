9de1a3ea673186abb869c2888e5693aaf51f4047
WJ-sr <wonjin.kim@seoulrobotics.org>
2021-01-25 15:05:52 +0900

    create console_output folder

---
+++ javascript/console_output/console_output.js
@@ -0,0 +1,44 @@
+const sensr = require('sensr_js_sdk');
+const output_sel = sensr.output_sel;
+const sensr_client = sensr.sensr_client;
+const yargs = sensr.yargs
+
+
+function run() {
+    parseCmdArgs();
+  }
+  
+  function fetchArgs() {
+    return yargs
+        .command('show',
+            'Specify type you want to visualize.', {
+              typename: {
+                description: 'type of data',
+                alias: 't',
+                type: 'string',
+              },
+              ipaddress: {
+                description: 'specify server',
+                alias: 's',
+                type: 'string',
+              }
+            })
+        .check(function(argv) {
+            return true;
+        })
+        .help()
+        .alias('help', 'h')
+        .argv;
+  }
+  
+  async function parseCmdArgs() {
+    const argv = fetchArgs();
+  
+    if (argv._.includes('show')) {
+        var client = sensr_client.SensrClient(argv.ipaddress);
+        output_sel.outputSelector(argv.typename, client);    
+    }
+}
+
+process.on('exit',  ()=>{client.disconnect()});
+run();
--- javascript/javascript_sdk/src/index.js
+++ javascript/javascript_sdk/src/index.js
@@ -2,41 +2,8 @@ const yargs = require('yargs');
 const output_sel = require('./output_selector.js')
 const sensr_client = require('./sensr_client');
 
-function run() {
-    parseCmdArgs();
-  }
-  
-  function fetchArgs() {
-    return yargs
-        .command('show',
-            'Specify type you want to visualize.', {
-              typename: {
-                description: 'type of data',
-                alias: 't',
-                type: 'string',
-              },
-              ipaddress: {
-                description: 'specify server',
-                alias: 's',
-                type: 'string',
-              }
-            })
-        .check(function(argv) {
-            return true;
-        })
-        .help()
-        .alias('help', 'h')
-        .argv;
-  }
-  
-  async function parseCmdArgs() {
-    const argv = fetchArgs();
-  
-    if (argv._.includes('show')) {
-        var client = sensr_client.SensrClient(argv.ipaddress);
-        output_sel.outputSelector(argv.typename, client);    
-    }
-}
-
-process.on('exit',  ()=>{client.disconnect()});
-run();
+module.exports ={
+  output_sel,
+  sensr_client,
+  yargs
+};
