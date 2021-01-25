e62183c50a48baba1c5d8c8951774c5ff8d691a7
kimwonjin97 <wonjin.kim@mail.utoronto.ca>
2021-01-25 11:32:37 +0900

    Delete and change folder names

--- javascript/javascript_sdk/src/index.js
+++ javascript/javascript_sdk/src/index.js
@@ -1,77 +1,39 @@
-'use strict';
-
-// Arg parsing
 const yargs = require('yargs');
-
-// Message receiver
-const receiver = require('./message_receiver');
-
-// Parsing
-const parsing = require('./parse_output');
-
-module.exports = {
-  run,
-  receiver,
-  parsing,
-};
-
+const output_sel = require('./output_selector.js')
 function run() {
-  parseCmdArgs();
-}
-
-function fetchArgs() {
-  return yargs
-      .command('parse',
-          'Parse from binary file.', {
-            filename: {
-              description: 'File to be parsed',
-              alias: 'f',
-              type: 'string',
-            },
-          })
-      .command('dump',
-          'Dump all receive messages from SENSR to a specified folder.', {
-            output_dir: {
-              description: 'Directory of where the received files are dumped',
-              alias: 'o',
-              type: 'string',
-            },
-          })
-      .check(function(argv) {
-        if (argv._.includes('parse') && argv.filename != undefined) {
-          return true;
-        } else if (argv._.includes('dump') && argv.output_dir != undefined) {
-          return true;
-        } else {
-          return false;
-        }
-      })
-      .help()
-      .alias('help', 'h')
-      .argv;
-}
-
-async function parseCmdArgs() {
-  const argv = fetchArgs();
-
-  if (argv._.includes('parse')) {
-    parsing.parseOutputFile(argv.filename);
+    parseCmdArgs();
   }
-
-  if (argv._.includes('dump')) {
-    const outputDir = argv.output_dir;
-    parsing.mkdir(outputDir);
-
-    console.log(`Dumping SENSR output to ${outputDir}...`);
-    const numExported = await receiveOutputs(outputDir);
-    console.log(`Finished dumping ${numExported} messages from SENSR.`);
+  
+  function fetchArgs() {
+    return yargs
+        .command('show',
+            'Spcifify Type you want to visualize.', {
+              typename: {
+                description: 'type of data',
+                alias: 't',
+                type: 'string',
+              },
+              ipaddress: {
+                description: 'specify server',
+                alias: 's',
+                type: 'string',
+              }
+            })
+        .check(function(argv) {
+            return true;
+        })
+        .help()
+        .alias('help', 'h')
+        .argv;
   }
+  
+  async function parseCmdArgs() {
+    const argv = fetchArgs();
+  
+    if (argv._.includes('show')) {
+        output_sel.outputSelector(argv.typename, argv.ipaddress);      
+    }
 }
 
-async function receiveOutputs(outputDir) {
-  const messageReceiver = new receiver.MessageReceiver();
-  const numExported = await messageReceiver.dumpAllReceived(outputDir);
-  return numExported;
-}
 
-
+run()
