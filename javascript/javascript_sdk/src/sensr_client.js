47c4ddf8e09d216696f45cd9a767d7aac2eb21a1
WJ-sr <wonjin.kim@seoulrobotics.org>
2021-04-13 14:06:46 +0900

    temp self signed certificate solved by disabliing the authorization

--- javascript/javascript_sdk/src/sensr_client.js
+++ javascript/javascript_sdk/src/sensr_client.js
@@ -3,14 +3,19 @@ const outputMsg = require('./../js_proto/sensr_proto/output_pb.js');
 const pointMsg = require('./../js_proto/sensr_proto/point_cloud_pb');
 const fs = require('fs');
 
+const cert_location = __dirname + '/../../../' + 'keys/sensr-sdk-ca.crt'
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
+
 
 const SensrClient = endpoint => {
     const result_socket = new WebSocket(`wss://${endpoint}:5050`, {
-      ca: fs.readFileSync(__dirname + '/../../../' + 'keys/sensr-sdk-ca.crt')
+      cert: fs.readFileSync(cert_location)
     });
     result_socket.binaryType = 'arraybuffer';
-    const point_socket = new WebSocket(`wss://${endpoint}:5051`);
+    const point_socket = new WebSocket(`wss://${endpoint}:5051`, {
+      cert: fs.readFileSync(cert_location)
+    });
     point_socket.binaryType = 'arraybuffer';
   
     let objectUpdateListener, pointCloudUpdateListener;
