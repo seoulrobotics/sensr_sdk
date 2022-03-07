607ddba83494c556f5d71f7d1733bae02b3f5bd8
Luke Kang <luke@seoulrobotics.org>
2022-03-07 15:10:11 +0900

    add reconnect in case error occur in python secure sample.

--- python/console_output_secure.py
+++ python/console_output_secure.py
@@ -20,6 +20,9 @@ class ZoneEvenListener(MessageListener):
                          use_ssl=True,
                          crt_file_path=cert_file_path)
 
+    def _on_error(self, message):
+        self.reconnect()
+
     def _on_get_output_message(self, message):
         assert isinstance(message, sensr_output.OutputMessage), "message should be of type OutputMessage"
 
@@ -38,7 +41,10 @@ class PointResultListener(MessageListener):
                          listener_type=ListenerType.POINT_RESULT,
                          use_ssl=True,
                          crt_file_path=cert_file_path)
-    
+
+    def _on_error(self, message):
+        self.reconnect()
+
     def _on_get_point_result(self, message):
         assert isinstance(message, sensr_pcloud.PointResult), "message should be of type PointResult"
         
@@ -61,6 +67,9 @@ class ObjectListener(MessageListener):
                          listener_type=ListenerType.OUTPUT_MESSAGE,
                          use_ssl=True,
                          crt_file_path=cert_file_path)
+
+    def _on_error(self, message):
+        self.reconnect()
 
     def _on_get_output_message(self, message):
         assert isinstance(message, sensr_output.OutputMessage), "message should be of type OutputMessage"
@@ -86,6 +95,9 @@ class HealthListener(MessageListener):
                          listener_type=ListenerType.OUTPUT_MESSAGE,
                          use_ssl=True,
                          crt_file_path=cert_file_path)
+
+    def _on_error(self, message):
+        self.reconnect()
 
     def _on_get_output_message(self, message):
         assert isinstance(message, sensr_output.OutputMessage), "message should be of type OutputMessage"
@@ -116,6 +128,9 @@ class TimeChecker(MessageListener):
                          listener_type=ListenerType.OUTPUT_MESSAGE,
                          use_ssl=True,
                          crt_file_path=cert_file_path)
+
+    def _on_error(self, message):
+        self.reconnect()
 
     def _on_get_output_message(self, message):
         assert isinstance(message, sensr_output.OutputMessage), "message should be of type OutputMessage"
